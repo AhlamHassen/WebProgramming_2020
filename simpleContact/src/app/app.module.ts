6ca488addbcba3d72a7995dca23724e33083edfb
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-08-12 22:13:00 +1000

    added contact component to the app component

--- simpleContact/src/app/app.module.ts
+++ simpleContact/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactComponentComponent } from './contact-component/contact-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponentComponent
   ],
   imports: [
     BrowserModule
