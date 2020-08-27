811547a5e736d0463a85d062f5bab2c60513bdf4
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-08-27 11:06:05 +1000

    Created Contact List Component

---
+++ simpleContact/src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactsService } from '../contacts.service'
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+
+  constructor(public ContactsService: ContactsService) { }
+
+  ngOnInit(): void {
+  }
+
+}
